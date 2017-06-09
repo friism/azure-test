ac4b71bdb929f84bd67f3e5416af52da723fbea0
Michael Friis <friism@gmail.com>
2017-06-09 13:50:18 -0700

    use dev for ucp install

--- install-ucp-mgr.sh
+++ install-ucp-mgr.sh
@@ -19,6 +19,7 @@ docker login -p $HUB_PASSWORD -u $HUB_USERNAME
 #install UCP
 docker run dockerorcadev/ucp:$UCP_VERSION images --list --image-version dev: | xargs -L 1 docker pull
 docker run --rm --name ucp \
+  -e REGISTRY_USERNAME=$HUB_USERNAME -e REGISTRY_PASSWORD=$HUB_PASSWORD \
   -v /var/run/docker.sock:/var/run/docker.sock \
-   dockerorcadev/ucp:$UCP_VERSION \
-   install --san $UCP_PUBLIC_FQDN --admin-password $UCP_ADMIN_PASSWORD --debug
+  dockerorcadev/ucp:$UCP_VERSION \
+  install --san $UCP_PUBLIC_FQDN --admin-password $UCP_ADMIN_PASSWORD --debug --image-version dev:
