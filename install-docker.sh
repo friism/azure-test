03f0468e45ba67db0500fe564650a9e831fb9191
Michael Friis <friism@gmail.com>
2017-08-09 17:51:14 -0700

    use correct EE versiond

--- install-docker.sh
+++ install-docker.sh
@@ -7,10 +7,10 @@ sudo apt-get install -y --no-install-recommends \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://storebits.docker.com/ee/ubuntu/sub-40aca674-a217-4881-93fe-021c5ab3c5db/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://storebits.docker.com/ee/ubuntu/sub-ccfd5e0b-f831-4f72-bdd1-4ec1df0db808/ubuntu \
    $(lsb_release -cs) \
    test"
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ee=$DOCKER_LINUX_VERSION~ubuntu
