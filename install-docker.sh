68e3515a6838be5d0653fc8739cc5f7e63d7cdb0
Michael Friis <friism@gmail.com>
2017-08-11 10:01:43 -0700

    use staging store

--- install-docker.sh
+++ install-docker.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y --no-install-recommends \
     software-properties-common
 curl -fsSL https://storebits.docker.com/ee/ubuntu/sub-40aca674-a217-4881-93fe-021c5ab3c5db/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://storebits.docker.com/ee/ubuntu/sub-ccfd5e0b-f831-4f72-bdd1-4ec1df0db808/ubuntu \
+   "deb [arch=amd64] https://storebits-stage.docker.com/ee/ubuntu/sub-2026a9b9-2aed-41a7-b138-45d2ea80d429/ubuntu \
    $(lsb_release -cs) \
    test"
 sudo apt-get update
